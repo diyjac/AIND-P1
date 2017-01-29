ed30c3bf3b049901eb39827f654eeb791fd89c32
diyjac <diyjac@gmail.com>
2017-01-28 19:39:09 -0500

    Update after first review

--- solution.py
+++ solution.py
@@ -33,32 +33,40 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     ##########################################################################################################
-    # we first find all possible naked twins values on the board
-    # then we use those values and isolate only the ones that appears more than once on the board: our candidates
-    # with our candidates, we iterate through our unitlist and find units that have square with the candidate values
-    # once we isolated the possible units with our candidate values, we get a list of all values for that unit
-    # with the unit value list, we confirm our naked-twins by verifying that they occur more than once in a unit and add them to our list.
-    # with the confirmed set of naked-twins, we search the units that have them and add them to a dictionary
+    # NOTE: we are using the same coding style as the AIND course materials to match, but are staging it to be
+    #       more readable, with longer variable names.
     ##########################################################################################################
 
+    # we first find all possible naked twins values on the board
     all_possible_naked_twins_values = [values[box] for box in values.keys() if len(values[box]) == 2]
+
+    # then we use those values and isolate only the ones that appears more than once on the board: our candidates
     candidate_naked_twins_values = [candidate for candidate in all_possible_naked_twins_values if all_possible_naked_twins_values.count(candidate)>1]
+
+    # with our candidates, we iterate through our unitlist and find units that have square with the candidate values
     units_with_candidates = [u for u in unitlist for candidate in candidate_naked_twins_values for s in u if values[s]==candidate]
+
+    # once we isolated the possible units with our candidate values, we get a list of all values for that unit
     units_with_candidates_values_list = dict(("+".join(u), [values[s] for s in u]) for u in units_with_candidates)
+
+    # with the unit value list, we confirm our naked-twins by verifying that they occur more than once in a unit and add them to our list.
     naked_twin_list = [twins for twins in candidate_naked_twins_values for u in units_with_candidates if units_with_candidates_values_list["+".join(u)].count(twins)>1] 
+
+    # with the confirmed set of naked-twins, we search the units that have them and add them to a dictionary
     units_with_naked_twins = dict(("+".join(u),naked) for u in units_with_candidates for naked in naked_twin_list if units_with_candidates_values_list["+".join(u)].count(naked)>1)
     
     # Eliminate the naked twins as possibilities for their peers
-    ##########################################################################################################
     # we already have the list of units and the naked-twins associated with them in our dictionary
-    # we just need to iterate through them and for squares that are not the ones with the naked-twins values,
-    # we remove the digits from them that are part of the naked-twins.  DONE!
-    ##########################################################################################################
     for naked_unit in units_with_naked_twins.keys():
         naked = units_with_naked_twins[naked_unit]
+
+        # we just need to iterate through them and for squares that are not the ones with the naked-twins values,
         for box in naked_unit.split('+'):
+
+            # if the boxes are not part of the naked-twins.
             if values[box] != naked:
                 for digit in naked:
+                    # remove the digits from boxes that are not part of the naked-twins.  DONE!
                     values = assign_value(values, box, values[box].replace(digit,''))
     return values
 
